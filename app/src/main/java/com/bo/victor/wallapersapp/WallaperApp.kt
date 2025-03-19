bc253add0d37ccb1deb644bcb701d903eee46325
Victor Sanjines <victorsanjines667@gmail.com>
2025-03-19 12:21:55 -0400

    First commit.

---
+++ app/src/main/java/com/bo/victor/wallapersapp/WallaperApp.kt
@@ -0,0 +1,14 @@
+package com.bo.victor.wallapersapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/****
+ * Project: WallapersApp
+ * From: com.bo.victor.wallapersapp
+ * Created by: Victor Sanjines Lopez on 19/3/2025 at 11:25
+ * More info: www.victorsanjines.com.bo
+ * All rights reserved 2025
+ ****/
+@HiltAndroidApp
+class WallaperApp : Application()
